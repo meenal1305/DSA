d0152e3e060b97794c6104f0d94f6f52e68c3839
meenal1305 <100236356+meenal1305@users.noreply.github.com>
2022-03-14 21:23:57 +0530

    Create hacker rank reverse array.c

---
+++ array/hacker rank reverse array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int array[n];
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&array[i]);
+}
+for(int i=n-1;i>=0;i--)
+{
+    printf("%d ",array[i]);
+}
+}
