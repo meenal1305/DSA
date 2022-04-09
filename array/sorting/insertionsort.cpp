071705418779dd6145c83b5972ee99ff9702a15b
meenal1305 <100236356+meenal1305@users.noreply.github.com>
2022-04-09 16:00:56 +0530

    Update insertionsort.cpp

--- array/sorting/insertionsort.cpp
+++ array/sorting/insertionsort.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 int main()
 {
-    int j;
-    int arr[5]={5,100,25,60,2};
-    for(int i=0;i<5;i++)
+    int n=5;
+    int arr[]={5,100,25,60,2};
+    for(int i=0;i<n;i++)
     {
-        for(int j=4;j>0;j--)
+        for(int j=i;j>0;j--)
         {
             if(arr[j]<arr[j-1])
-        swap(arr[j],arr[j-1]);
+            {
+               swap(arr[j],arr[j-1]);
+            }
+            else
+            {
+                break;
+            }
+        }
     }
-    }
-    for(int k=0;k<5;k++)
+    for(int k=0;k<n;k++)
     {
         cout<<arr[k]<<" ";
     }
